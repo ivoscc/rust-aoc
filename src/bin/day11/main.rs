26385203d6bf1c0f226058b90b5f44f5e495336a
Ivo Sánchez Checa <ivoscc@gmail.com>
2021-11-04 20:26:40 +0100

    save

--- src/bin/day11/main.rs
+++ src/bin/day11/main.rs
@@ -1,15 +1,47 @@
+use std::collections::HashSet;
+
 fn main() {
     let input = include_str!("input.txt");
     let output_part_1 = part_1(input);
     println!("Part 1 output is {:?}", output_part_1);
-    let output_part_2 = part_2(input);
+    let output_part_2 = part_2(&output_part_1);
     println!("Part 2 output is {:?}", output_part_2);
-    // assert_eq!(_, output_part_1);
-    // assert_eq!(_, output_part_2);
+    assert_eq!("hepxxyzz", output_part_1);
+    assert_eq!("heqaabcc", output_part_2);
 }
 
-fn is_valid_password(input: &str) -> bool {
-    false
+fn is_valid_password(
+    input: &str,
+    invalid_characters: &HashSet<char>,
+    repeats: &HashSet<[char; 2]>,
+    triplets: &HashSet<[char; 3]>,
+) -> bool {
+    for character in input.chars() {
+        if invalid_characters.contains(&character) {
+            return false;
+        }
+    }
+
+    let mut contains_triplet = false;
+    for triplet in input.chars().collect::<Vec<char>>().windows(3) {
+        if triplets.contains(triplet) {
+            contains_triplet = true;
+            break;
+        }
+    }
+
+    let mut contains_repeated_pair = 0;
+    let mut last_repeat = false;
+    for pair in input.chars().collect::<Vec<char>>().windows(2) {
+        if repeats.contains(pair) && !last_repeat {
+            contains_repeated_pair += 1;
+            last_repeat = true;
+        } else {
+            last_repeat = false;
+        }
+    }
+
+    contains_triplet && (contains_repeated_pair > 1)
 }
 
 fn next_password(password: &str) -> String {
@@ -25,30 +57,21 @@ fn next_password(password: &str) -> String {
     let mut remainder: u8 = 1;
 
     while remainder > 0 && output.len() < 8 {
-        if let Some(bytechar) = bytes_iterator.next() {
-            let next_bytechar = bytechar + remainder;
-            remainder = if next_bytechar >= max_bytechar {
-                next_bytechar - max_bytechar
-            } else {
-                0
-            };
-            if remainder > 0 {
-                output.push(min_bytechar as char);
-            } else {
-                output.push(next_bytechar as char)
-            }
+        let sum_bytechar = if let Some(bytechar) = bytes_iterator.next() {
+            bytechar + remainder
         } else {
-            let next_bytechar = min_bytechar + remainder;
-            remainder = if next_bytechar >= max_bytechar {
-                next_bytechar - max_bytechar
-            } else {
-                0
-            };
-        }
+            min_bytechar + remainder
+        };
+        remainder = if sum_bytechar >= max_bytechar {
+            sum_bytechar - max_bytechar
+        } else {
+            0
+        };
+
         if remainder > 0 {
             output.push(min_bytechar as char);
         } else {
-            output.push(next_bytechar as char)
+            output.push(sum_bytechar as char)
         }
     }
 
@@ -60,21 +83,35 @@ fn next_password(password: &str) -> String {
 }
 
 fn part_1(input: &str) -> String {
-    let input = "zzzzz";
+    let repeated_pairs: HashSet<[char; 2]> =
+        HashSet::from_iter(('a'..='z').map(|character| [character, character]));
+
+    let ascending_triplets: HashSet<[char; 3]> = HashSet::from_iter(
+        ('a'..='z')
+            .collect::<Vec<char>>()
+            .windows(3)
+            .map(|triplet| [triplet[0], triplet[1], triplet[2]]),
+    );
+
+    let invalid_characters: HashSet<char> = HashSet::from_iter(['i', 'l', 'o'].into_iter());
+
     let mut password: String = input.trim().into();
-    // password = next_password(&password);
-    println!("Password is {}", password);
-    for _ in 0..4 {
+
+    loop {
         password = next_password(&password);
-        println!("Next password is {}", password);
+        if is_valid_password(
+            &password,
+            &invalid_characters,
+            &repeated_pairs,
+            &ascending_triplets,
+        ) {
+            break;
+        }
     }
-    // loop {
-    //     password = next_password(&password);
-    //     // if is_valid_password(&password) {
-    //     //     break;
-    //     // }
-    // }
+
     password
 }
 
-fn part_2(input: &str) -> () {}
+fn part_2(input: &str) -> String {
+    part_1(input)
+}
