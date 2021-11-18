d99df191dc92f22e992e0269b58217261d4a2a40
Ivo Sánchez Checa <ivoscc@gmail.com>
2021-11-18 21:41:56 +0100

    Add 2016 directory

---
+++ aoc-2016/src/bin/day01/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // let input = include_str!("input.txt");
+    let input = "";
+    let output_part_1 = part_1(input);
+    println!("Part 1 output is {:?}", output_part_1);
+    let output_part_2 = part_2(input);
+    println!("Part 2 output is {:?}", output_part_2);
+    // assert_eq!(_, output_part_1);
+    // assert_eq!(_, output_part_2);
+}
+
+fn part_1(input: &str) -> () {}
+
+fn part_2(input: &str) -> () {}
